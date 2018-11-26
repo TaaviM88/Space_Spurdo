8dad67afdba6be812d12f8693e41ae6408b598ef
TaaviM88Laptop <juomakuski@hotmail.com>
2018-11-26 20:30:57 +0200

    Playermovement created player created some tmp materials demoscene created

---
+++ Space_spurdo_racer/Assets/Scripts/CylinderScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderScript : MonoBehaviour
+{
+    public GameObject player;
+    public bool firstOne = false;
+    public int towerIndex = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.z > (transform.position.z + 30))
+        {
+            MoveCylinder();
+        }
+    }
+
+    public void MoveCylinder()
+    {
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + towerIndex);
+        transform.Rotate(new Vector3(transform.rotation.x + 180, transform.rotation.y, 0));
+
+
+    }
+}
