05664c79b74e3535473af3577ad322b87768eb0f
AKSELI-LAPTOP\Akseli <azzeli91@gmail.com>
2018-12-07 10:18:51 +0200

    Added UI texture for speedometer.

--- Space_spurdo_racer/Assets/Scripts/UIManager.cs
+++ Space_spurdo_racer/Assets/Scripts/UIManager.cs
@@ -4,20 +4,30 @@ using TMPro;
 
 public class UIManager : MonoBehaviour {
 
-    Rigidbody _rb;
+    private Image image;
     public TMP_Text speedometer;
-    int speed = 0;
 
+    Rigidbody _rb;
+    public Rigidbody objectToMeasure;
+    
+    public float maxVelocity = 10.0f;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        image = GetComponent<Image>();
         _rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        image.fillAmount = objectToMeasure.velocity.magnitude / maxVelocity;
+        
         speedometer.text = string.Format("{0:0.00}", _rb.velocity.magnitude);
     }
 }
